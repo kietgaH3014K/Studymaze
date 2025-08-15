a06638ecdada6b22bb1f4a813c4c55dae1d61578
kietgaH3014K <ntkiet5102008@gmail.com>
2025-08-15 09:32:24 +0700

    Update Dashboard.jsx

--- studyapp_maze/src/pages/Dashboard.jsx
+++ studyapp_maze/src/pages/Dashboard.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import "./Dashboard.css";
 
-export default function Dashboard() 
-  
- {
+export default function Dashboard() {
+  const cards = [
+    { image: "/image/chatAI.png", title: "Chat với AI" },
+    { image: "/image/danhgia.jpg", title: "Phiếu đánh giá học tập" },
+    { image: "/image/lichhoc.jpg", title: "lịch học" },
+  ];
+
   return (
     <section className="dashboard-hero">
-      
-   
-        <div className="after-image">
+      <div className="after-image">
         <h2>Dashboard - Tổng quan</h2>
         <p>Chào mừng bạn đến với ứng dụng StudyApp!</p>
       </div>
 
-
+      <div className="card-container">
+        {cards.map((card, index) => (
+          <div className="card-frame" key={index}>
+            <img src={card.image} alt={card.title} />
+            <p className="card-title">{card.title}</p>
+          </div>
+        ))}
+      </div>
     </section>
-    
-   
-     
   );
 }
+
